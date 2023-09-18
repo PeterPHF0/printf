0a91d333617fc19fb314ee570c617e72bf8bc5b7
MinaEhab77 <fouadattia09@gmail.com>
2023-09-18 11:36:06 -0700

    updated

--- main.h
+++ main.h
@@ -1,28 +1,51 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <limits.h>
+
 #define OUTPUT_BUF_SIZE 1024
 #define BUF_FLUSH -1
-#define PARAMS_INIT (0,0,0,0,0,0,0,0,0,0)
+
+#define PARAMS_INIT (0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
 #define NULL_STRING "(null)"
 
+/**
+* struct parameters - parameters struct
+*
+* @unsign: flag if unsigned value
+*
+* @plus_flag: on if plus flag specified
+* @space_flag: on if hashtag flag specified
+* @hashtag_flag: on if flag specified
+* @zero_flag: on if flag specified
+* @minus_flag: on if flag specified
+*
+* @width: field width specified
+* @precision: field precision specified
+*
+* @h_modifier: on if h_modifier is specified
+* @l_modifier: on if l modifier is specified
+*/
 typedef struct parameters
 {
-unsigned int unsign :1;
+unsigned int unsign : 1;
 
-unsigned int width :1; 
-unsigned int precision :1;
+unsigned int width : 1;
+unsigned int precision : 1;
 
-unsigned int plus_flag :1;
-unsigned int space_flag :1;
-unsigned int hashtag_flag :1;
-unsigned int zero_flag :1;
-unsigned int minus_flag :1;
+unsigned int plus_flag : 1;
+unsigned int space_flag : 1;
+unsigned int hashtag_flag : 1;
+unsigned int zero_flag : 1;
+unsigned int minus_flag : 1;
 
-unsigned int h_modifier :1;
-unsigned int l_modifier :1;
+unsigned int h_modifier : 1;
+unsigned int l_modifier : 1;
 } params_t;
 
 /**
