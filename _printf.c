729a87544aeca0437b86956702a53ee732968665
MinaEhab77 <fouadattia09@gmail.com>
2023-09-18 12:18:47 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -1,42 +1,47 @@
-#include<main.h>
+#include "main.h"
+
 /**
- * main - Entry point
- *
- * Description: use sizeof
- *
- * Return: 0 (Success)
+* printf - prints anything
+* @format: the format string
+* Return: number of bytes printed
 */
-
-void _printf(const char* format, ...)
+int printf(const char *format, ...)
 {
-	va_list args;
-	va_start(args, format);
+int sum = 0;
+va_list ptr;
+char *p, *start;
+params_t params = PARAMS_INIT;
 
-	while (*format) {
-		if (*format == '%') {
-			format++; // Move to the character after '%'
-			switch (*format) {
-				case 'd': {
-					int value = va_arg(args, int);
-					printf("%d", value);
-					break;
-					  }
-				case 's': {
-					char* str = va_arg(args, char*);
-					printf("%s", str);
-					break;
-					  }
-				default:
-					  putchar(*format);
-			}
-		} else
-		{
-			putchar(*format);
-		}
-		format++;
-	}
-	va_end(args);
+va_start(ptr, format);
+
+if (!format || (format[0] == '%' && !format[1]))
+return (-1);
+if (format[0] == '%' && format[1] == ' ' && !format[2])
+return (-1);
+for (p = (char *)format; *p; p++)
+{
+init_params(&params, ptr);
+if (*p != '%')
+sum += _putchar(*p);
+continue;
 }
-
-
+start = p;
+p++;
+while (get_flag(p, &params))
+{
+p++
+}
+p = get_width(p, &params, ptr);
+p = get_precision(p, &params, ptr);
+if (get_modifier(p, &params))
+p++;
+if (!get specifier(p))
+sum += print_from_to(start, p, params.1_modifier ||
+						params.h_modifier ? p - 1 : 0);
+else
+sum += get_print_func(p, ptr, &params);
+}
+_putchar(BUF_FLUSH);
+va_end(ap);
+return (sum);
 
