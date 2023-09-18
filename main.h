69e8b42472311db755eebb4dd528d2b14ef16854
MinaEhab77 <fouadattia09@gmail.com>
2023-09-18 08:47:19 -0700

    updated

--- main.h
+++ main.h
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
+#define PARAMS_INIT (0,0,0,0,0,0,0,0,0,0)
 
 /**
  * struct specifier - struct token/
