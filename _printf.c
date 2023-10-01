e639adeec6fc81fb6871d34c4fe6a8c225631c74
MinaEhab77 <fouadattia09@gmail.com>
2023-10-01 11:19:41 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -1,125 +1,61 @@
-#include "main.h"
-
-
-void print_buffer(char buffer[], int *buff_ind);
-
+#include "holberton.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
-<<<<<<< HEAD
- * _printf - Printf fun.
- * @format: just a format.
- * Return: characters to be printed.
-=======
- * _printf - Printf funvsdv sdv sdv vction
- * @format: vsd svsd sdvopjpovd.
- * Return: Printed dinovsdn sknv sndmvsdvmsdv.
->>>>>>> 1b5a92437ebf5ab065e6a1f612f42d492a77fce7
+ * _printf - custom printf function
+ * @format: string to be printed
+ *
+ * Return: number of characters printed
  */
-
+ 
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
-if (format == NULL)
-return (-1);
-va_start(list, format);
-for (i = 0; format && format[i] != '\0'; i++)
+int i = 0, count = 0;
+char *str;
+va_list args;
+
+va_start(args, format);
+for (i = 0; format[i] != '\0'; i++)
 {
-if (format[i] != '%')
+if (format[i] == '%')
 {
-buffer[buff_ind++] = format[i];
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-=======
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
->>>>>>> 1b5a92437ebf5ab065e6a1f612f42d492a77fce7
-
-printed_chars++;
+i++;
+switch (format[i])
+{
+case 'c':
+putchar(va_arg(args, int));
+count++;
+break;
+case 's':
+str = va_arg(args, char*);
+if (str == NULL)
+{
+str = "(null)";
+}
+while (*str != '\0')
+{
+putchar(*str);
+str++;
+count++;
+}
+break;
+case '%':
+putchar('%');
+count++;
+break;
+default:
+putchar('%');
+putchar(format[i]);
+count += 2;
+}
 }
 else
 {
-print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &i);
-width = get_width(format, &i, list);
-precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-++i;
-printed = handle_print(format, &i, list, buffer,
-flags, width, precision, size);
-if (printed == -1)
-return (-1);
-printed_chars += printed;
+putchar(format[i]);
+count++;
 }
 }
-print_buffer(buffer, &buff_ind);
-va_end(list);
-return (printed_chars);
-}
-
-/* next function to print buffer */
-
-/**
-<<<<<<< HEAD
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of characters
- * @buff_ind: Index at which to add next char.
-=======
- * print_buffer - Prints the contents knvsdk mjdsklcns f it exist
- * @buffer: Array of chars ksndvksd ndsnvsjdv bjksdv.
- * @buff_ind: add next charn lkj lk nmdnvsn jnvs, represents the length.
->>>>>>> 1b5a92437ebf5ab065e6a1f612f42d492a77fce7
- */
-
-
-void print_buffer(char buffer[], int *buff_ind)
-{
-<<<<<<< HEAD
-if (*buff_ind > 0)
-write(1, &buffer[0], *buff_ind);
-*buff_ind = 0;
-=======
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-	*buff_ind = 0;
->>>>>>> 1b5a92437ebf5ab065e6a1f612f42d492a77fce7
+va_end(args);
+return (count);
 }
