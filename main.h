4ec2aa4d484ad13cae84f973b08d386cb33dc452
PeterPHF0 <138907638+PeterPHF0@users.noreply.github.com>
2023-10-11 18:57:04 +0300

    Update main.h

--- main.h
+++ main.h
@@ -7,13 +7,13 @@
 #include <stdlib.h>
 
 char *intToString(int num);
-va_list print_char(va_list ls);
-va_list print_s(va_list ls);
-va_list print_int(va_list ls);
+char *print_char(va_list ls);
+char *print_s(va_list ls);
+int *print_int(va_list ls);
 int *generate_array(int num);
 int get_arr_len(int *arr);
-va_list binary(va_list ls);
-va_list get_format(const char *str, int i, va_list ls);
+int *binary(va_list ls);
+void *get_format(const char *str, int i, va_list ls);
 void _put(const char *str, va_list ls);
 int _printf(const char *format, ...);
 
